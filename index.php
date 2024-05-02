12af7d420e7470e84b89e7fda837868ea9a22dd1
Jorge <jb2017059611@virtual.upt.pe>
2024-05-01 23:27:50 -0500

    Solucion 1

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-// Define a constant for the directory of the views
-define('VISTAS_DIR', "./vistas/");
+// Define una constante para la ruta de las vistas
+define('VISTAS_PATH', "./vistas/");
 
 $INCLUDE_ALLOW_LIST = [
      "home.php",
@@ -11,36 +11,34 @@ $INCLUDE_ALLOW_LIST = [
 
 $vista = $_GET["vista"];
 if (in_array($vista, $INCLUDE_ALLOW_LIST)) {
-   require_once VISTAS_DIR . $vista . ".php";
+   require_once VISTAS_PATH . $vista . ".php";
 }
-
 require_once "./inc/session_start.php";
 ?>
 <!DOCTYPE html>
 <html>
     <head>
-        <?php include_once "./inc/head.php"; ?>
+        <?php require_once "./inc/head.php"; ?>
     </head>
     <body>
         <?php
             if(!isset($_GET['vista']) || $_GET['vista']==""){
                 $_GET['vista']="login";
             }
-            if(is_file(
-VISTAS_DIR . $_GET['vista'] . ".php") && $_GET['vista']!="login" && $_GET['vista']!="404"){
-                /*== Cerrar sesion ==*/
+            if(is_file(VISTAS_PATH . $_GET['vista'] . ".php") && $_GET['vista']!="login" && $_GET['vista']!="404"){
+                /*== Cerrar sesión ==*/
                 if((!isset($_SESSION['id']) || $_SESSION['id']=="") || (!isset($_SESSION['usuario']) || $_SESSION['usuario']=="")){
-                    include_once VISTAS_DIR . "logout.php";
+                    require_once VISTAS_PATH . "logout.php";
                     exit();
                 }
-                include_once "./inc/navbar.php";
-                include_once VISTAS_DIR . $_GET['vista'] . ".php";
-                include_once "./inc/script.php";
-            }else{
+                require_once "./inc/navbar.php";
+                require_once VISTAS_PATH . $_GET['vista'] . ".php";
+                require_once "./inc/script.php";
+            } else {
                 if($_GET['vista']=="login"){
-                    include_once VISTAS_DIR . "login.php";
-                }else{
-                    include_once VISTAS_DIR . "404.php";
+                    require_once VISTAS_PATH . "login.php";
+                } else {
+                    require_once VISTAS_PATH . "404.php";
                 }
             }
         ?>
