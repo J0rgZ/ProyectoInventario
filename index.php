c9cd13701e54ef4a62797b868be1536b74cd27a8
Jorge <jb2017059611@virtual.upt.pe>
2024-05-01 23:09:08 -0500

    Solucion 1

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
-<?php require "./inc/session_start.php"; ?>
+<?php
+$INCLUDE_ALLOW_LIST = [
+     "home.php",
+     "dashboard.php",
+     "profile.php",
+     "settings.php"
+];
+
+$vista = $_GET["vista"];
+if (in_array($vista, $INCLUDE_ALLOW_LIST)) {
+   include "./vistas/".$vista.".php";
+}
+
+require "./inc/session_start.php";
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -6,14 +20,11 @@
     </head>
     <body>
         <?php
-
             if(!isset($_GET['vista']) || $_GET['vista']==""){
                 $_GET['vista']="login";
             }
 
-
             if(is_file("./vistas/".$_GET['vista'].".php") && $_GET['vista']!="login" && $_GET['vista']!="404"){
-
                 /*== Cerrar sesion ==*/
                 if((!isset($_SESSION['id']) || $_SESSION['id']=="") || (!isset($_SESSION['usuario']) || $_SESSION['usuario']=="")){
                     include "./vistas/logout.php";
@@ -21,11 +32,8 @@
                 }
 
                 include "./inc/navbar.php";
-
                 include "./vistas/".$_GET['vista'].".php";
-
                 include "./inc/script.php";
-
             }else{
                 if($_GET['vista']=="login"){
                     include "./vistas/login.php";
